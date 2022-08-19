0e6dcae7c9fb75a243dd83e683fb0ceec66122f1
david-commit <david.ondiege@students.moringaschool.com>
2022-08-19 23:11:25 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+function writeCards(names, event){
+    const array = [];
+    for (let i = 0; i < names.length; i++){
+        array.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        
+        
+    }
+    return array;
+}
+
+function countDown(int){
+    while (int >= 0){
+        console.log(int--)
+    }
+}
